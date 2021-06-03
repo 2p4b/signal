81f148832d57766b91b8bb93dd8eb54ef80685ef
mfoncho <akonumboche@gmail.com>
2021-06-03 04:34:48 -0600

    assert streams

--- lib/signal/test/test_case.ex
+++ lib/signal/test/test_case.ex
@@ -1,12 +1,17 @@
 defmodule Signal.TestCase do
     @moduledoc """
-    This module defines the test case to be used by aggregate tests.
+    This module defines the test case to be used by tests.
     """
 
     use ExUnit.CaseTemplate
 
     using do
         quote do
+
+            # assert command or event stream
+            defp assert_stream(stream, payload) do
+                assert stream = Signal.Stream.stream(payload)
+            end
 
             # assert that the expected events are returned when the given commands
             # have been executed
@@ -54,6 +59,7 @@ defmodule Signal.TestCase do
                 Signal.Command.Executor.execute(command, params)
             end
 
+            # handle a command
             defp handle(command) when is_struct(command) do
                 handle(command, nil, %{})
             end
@@ -67,7 +73,7 @@ defmodule Signal.TestCase do
             when is_struct(command) and is_map(params) do
                 handle(command, nil, %{})
             end
-            # handle one or more command
+
             defp handle(command, aggregate, params \\ %{})
             when is_struct(command) and (is_struct(aggregate) or is_nil(aggregate)) do
 
@@ -106,29 +112,42 @@ defmodule Signal.TestCase do
                 end
             end
 
+            # apply the given events to the aggregate state
             defp evolve(aggregate, event, from \\ 0)
 
-            # apply the given events to the aggregate state
             defp evolve(aggregate, event, from) when is_struct(event) do
                 evolve(aggregate, [event], from)
             end
 
             defp evolve(aggregate, events, from) when is_list(events) do
-                Enum.reduce(events, {aggregate, from}, fn event, {aggregate, index} -> 
+                {aggregate, _version} =
+                    Enum.reduce(events, {aggregate, from}, fn event, {aggregate, index} -> 
 
-                    unless reduces?(aggregate, event) do
-                        {aggregate_module, _sid} = Signal.Stream.stream(event)
-                        raise ArgumentError, message: """
-                            Event requires aggregate of type 
-                            #{inspect(aggregate_module)}
-                            #{inspect(event)} 
-                        """
-                    end
+                        unless reduces?(aggregate, event) do
+                            {aggregate_module, _sid} = Signal.Stream.stream(event)
+                            raise ArgumentError, message: """
+                                Event requires aggregate of type 
+                                #{inspect(aggregate_module)}
+                                #{inspect(event)} 
+                            """
+                        end
 
-                    version = index + 1
-                    meta =  struct(Signal.Events.Event.Meta, [number: version])
-                    {Signal.Stream.Reducer.apply(aggregate, meta, event), version}
-                end)
+                        version = index + 1
+                        meta =  struct(Signal.Events.Event.Meta, [number: version])
+                        {Signal.Stream.Reducer.apply(aggregate, meta, event), version}
+                    end)
+                aggregate
+            end
+
+            # check if aggerate reduces an event or command
+            defp reduces?(aggregate, payload) 
+            when is_atom(aggregate) and is_struct(payload) do
+                {aggregate_module, _sid} = Signal.Stream.stream(payload)
+                if aggregate_module != aggregate do
+                    false
+                else
+                    true
+                end
             end
 
             defp reduces?(aggregate, payload) 
