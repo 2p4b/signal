96c27b8c69703cca13c0bb3b4d41762ab197017e
mfoncho <akonumboche@gmail.com>
2022-06-23 02:20:08 -0600

    update auto apply events on non strict aggregates

--- lib/signal/aggregate.ex
+++ lib/signal/aggregate.ex
@@ -1,9 +1,11 @@
 defmodule Signal.Aggregate do
 
-    defmacro __using__(_opts) do
+    defmacro __using__(opts) do
+        strict = Keyword.get(opts, :strict, false)
         quote do
             use Blueprint.Struct
             @module __MODULE__
+            @apply_strict unquote(strict)
             @before_compile unquote(__MODULE__)
         end
     end
@@ -19,6 +21,12 @@ defmodule Signal.Aggregate do
                             Kernel.apply(@pmodule, :apply, [event, meta, agg])
                         end
                     end
+                end
+            end
+
+            if @apply_strict === false do
+                def apply(_event, _meta, state) do
+                    {:ok, state}
                 end
             end
         end
