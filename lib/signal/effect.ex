e74ed3804200787298284c70d95f4c0172ed9a13
mfoncho <akonumboche@gmail.com>
2023-01-27 01:06:02 -0600

    add uuid to Signal.Effect struct

--- lib/signal/effect.ex
+++ lib/signal/effect.ex
@@ -1,8 +1,19 @@
 defmodule Signal.Effect do
-    defstruct [:id, :namespace, :object, :number]
+    defstruct [:uuid, :id, :namespace, :object, :number]
 
-    def new(opts) do
-        struct(__MODULE__, opts)
+    def new(opts) when is_list(opts) do
+        uuid = 
+            opts 
+            |> Enum.into(%{}) 
+            |> create_uuid()
+        struct(__MODULE__, Keyword.merge(opts, [uuid: uuid]))
     end
+
+    defp create_uuid(%{namespace: namespace, id: id}) do
+        :oid
+        |> UUID.uuid5(namespace)
+        |> UUID.uuid5(id)
+    end
+
 
 end
