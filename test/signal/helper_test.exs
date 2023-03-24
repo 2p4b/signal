0118ce1c9e90f543a75b2c10f28450a95666e839
mfoncho <akonumboche@gmail.com>
2023-03-24 07:10:38 -0600

    fix sub stream id generation

--- test/signal/helper_test.exs
+++ test/signal/helper_test.exs
@@ -3,7 +3,53 @@ defmodule Signal.HelperTest do
     use ExUnit.Case
     alias Signal.Helper
 
+    defmodule Sample.Command do
+        use Signal.Command,
+            stream: {StreamType, :parent_id, name: "subname"}
+
+        schema required: true do
+            field :parent_id, :uuid
+        end
+
+    end
+
+    defmodule Sample.SHA.Command do
+        use Signal.Command,
+            stream: {StreamType, :parent_id, name: "subname", hash: :sha}
+
+        schema required: true do
+            field :parent_id, :uuid
+        end
+    end
+
     describe "Signal.Helper" do
+
+        test "Signal.Stream.stream for sub stream object default hash" do
+            sub_name = "subname"
+            root_id = UUID.uuid1()
+            stream_obj = %Sample.Command{parent_id: root_id}
+            {sample_stream_id, sample_stream_type} = Signal.Stream.stream(stream_obj)
+            opts = [name: sub_name]
+            stream_id = Helper.stream_id(root_id, opts)
+            assert stream_id !== root_id
+            assert stream_id === Helper.namespaced_uuid(root_id, sub_name, :md5)
+            assert stream_id === sample_stream_id
+            assert StreamType === sample_stream_type
+        end
+
+        test "Signal.Stream.stream for sub stream object hash sha" do
+            sub_name = "subname"
+            root_id = UUID.uuid1()
+            stream_obj = %Sample.SHA.Command{parent_id: root_id}
+            {sample_stream_id, sample_stream_type} = Signal.Stream.stream(stream_obj)
+            opts = [name: sub_name, hash: :sha]
+            stream_id = Helper.stream_id(root_id, opts)
+            assert stream_id !== root_id
+            assert stream_id === Helper.namespaced_uuid(root_id, sub_name, :sha)
+            assert stream_id === sample_stream_id
+            assert StreamType === sample_stream_type
+        end
+
         test "stream_id/2 default md5" do
             sub_name = "sub"
             root_id = UUID.uuid1()
