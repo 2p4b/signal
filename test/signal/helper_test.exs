a49b06d06c389ee757ec1d10885505229d086271
mfoncho <akonumboche@gmail.com>
2023-03-24 04:59:49 -0600

    add Signal.Helper.stream_id/2 simple test

--- test/signal/helper_test.exs
+++ test/signal/helper_test.exs
@@ -1,24 +1,34 @@
 defmodule Signal.HelperTest do
 
     use ExUnit.Case
+    alias Signal.Helper
 
     describe "Signal.Helper" do
-        test "stream_tupe/3" do
-            assert {"stream_id", __MODULE__} ==
-                __MODULE__
-                |> Signal.Helper.stream_tuple("stream_id", [])
+        test "stream_id/2 default md5" do
+            sub_name = "sub"
+            root_id = UUID.uuid1()
+            opts = [name: sub_name]
+            stream_id = Helper.stream_id(root_id, opts)
+            assert stream_id !== root_id
+            assert stream_id === Helper.namespaced_uuid(root_id, sub_name, :md5)
         end
 
-        test "stream_tupe/3 with prefix" do
-            assert {"prefix:stream_id", __MODULE__} ==
-                __MODULE__
-                |> Signal.Helper.stream_tuple("stream_id", [prefix: "prefix:"])
+        test "stream_id/2 :md5" do
+            sub_name = "sub"
+            root_id = UUID.uuid1()
+            opts = [name: sub_name, hash: :md5]
+            stream_id = Helper.stream_id(root_id, opts)
+            assert stream_id !== root_id
+            assert stream_id === Helper.namespaced_uuid(root_id, sub_name, :md5)
         end
 
-        test "module_to_string/1" do
-            assert "Signal.HelperTest" ==
-                __MODULE__
-                |> Signal.Helper.module_to_string()
+        test "stream_id/2 :sha" do
+            sub_name = "sub"
+            root_id = UUID.uuid1()
+            opts = [name: sub_name, hash: :sha]
+            stream_id = Helper.stream_id(root_id, opts)
+            assert stream_id !== root_id
+            assert stream_id === Helper.namespaced_uuid(root_id, sub_name, :sha)
         end
 
         test "string_to_module/1" do
