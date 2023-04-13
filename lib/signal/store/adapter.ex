335f3e2f3c4343c026b1a29a4c177b0b25551ea3
mfoncho <akonumboche@gmail.com>
2023-04-13 00:46:02 -0600

    add app to store opts

--- lib/signal/store/adapter.ex
+++ lib/signal/store/adapter.ex
@@ -5,36 +5,43 @@ defmodule Signal.Store.Adapter do
     end
 
     def get_effect(app, uuid, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :get_effect, [uuid, opts])
     end
 
     def save_effect(app, effect, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :save_effect, [effect, opts])
     end
 
     def list_effects(app, namespace, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :list_effects, [namespace, opts])
     end
 
     def delete_effect(app, uuid, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :delete_effect, [uuid, opts])
     end
 
     def commit_transaction(app, transaction, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :commit_transaction, [transaction, opts])
     end
 
     def get_cursor(app, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :get_cursor, [opts])
     end
 
     def get_event(app, number, opts\\[]) do 
+        opts = opts_add(opts, :app, app)
         opts = Keyword.merge(opts, [range: [number, number]])
         case list_events(app, opts) do
             [head| _] ->
@@ -50,6 +57,7 @@ defmodule Signal.Store.Adapter do
     end
 
     def get_stream_event(app, stream_id, version, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         opts = Keyword.merge(opts, [range: [version, version]])
         case list_stream_events(app, stream_id, opts) do
             [head| _] ->
@@ -65,53 +73,67 @@ defmodule Signal.Store.Adapter do
     end
 
     def read_events(app, callback, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :read_events, [callback, opts])
     end
 
     def read_stream_events(app, stream_id, callback, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :read_stream_events, [stream_id, callback, opts])
     end
 
     def list_events(app, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :list_events, [opts])
     end
 
     def list_stream_events(app, stream_id, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :list_stream_events, [stream_id, opts])
     end
 
     def handler_position(app, handle, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :handler_position, [handle, opts])
     end
 
     def handler_acknowledge(app, handle, number, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :handler_acknowledge, [handle, number, opts])
     end
 
     def get_snapshot(app, id, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :get_snapshot, [id, opts])
     end
 
     def delete_snapshot(app, id, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :delete_snapshot, [id, opts])
     end
 
     def record_snapshot(app, snapshot, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :record_snapshot, [snapshot, opts])
     end
 
     def stream_position(app, stream, opts\\[]) do
+        opts = opts_add(opts, :app, app)
         store = application_store(app)
         Kernel.apply(store, :stream_position, [stream, opts])
+    end
+
+    defp opts_add(opts, keyword, value) when is_list(opts) and is_atom(keyword) do
+        Keyword.put(opts, keyword, value)
     end
 
 end
