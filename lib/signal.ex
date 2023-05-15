cfcf2a94ec3812846fd980852aad63caeb9ef7ae
mfoncho <akonumboche@gmail.com>
2023-05-14 23:07:37 -0600

    signal application helpers @todo

--- lib/signal.ex
+++ lib/signal.ex
@@ -1,15 +1,5 @@
 defmodule Signal do
-    # See https://hexdocs.pm/elixir/Application.html
-    # for more information on OTP Applications
+    # @todo impl application helpers
     @moduledoc false
-
-    def start(_type, _args) do
-        children = [
-            #Signal.Application
-        ]
-
-        opts = [strategy: :one_for_one, name: Signal.Supervisor]
-        Supervisor.start_link(children, opts)
-    end
 
 end
