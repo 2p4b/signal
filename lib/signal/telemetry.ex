49bcd027627ede2f552be1abda974df1404da949
mfoncho <akonumboche@gmail.com>
2024-04-07 10:16:37 -0600

    Refactor Signal.Telemetry for enhanced functionality and typo correction
    - Corrected module name typo from Signal.Telemety to Signal.Telemetry.
    - Modified  and  functions to accept lists for signal parameter and enforce guards.
    - Replaced  with  for clearer action representation in telemetry events.
    - Introduced  function to convert module names to telemetry paths.
    - Added macros  and  for easier integration of telemetry functionality.

--- lib/signal/telemetry.ex
+++ lib/signal/telemetry.ex
@@ -1,30 +1,45 @@
-defmodule Signal.Telemety do
-    @prefix :signal
+defmodule Signal.Telemetry do
     @start :start
     @stop :stop
 
-    def start(signal, metadata \\ %{}, measurements \\ %{}) do
+    def start(signal, metadata \\ %{}, measurements \\ %{}) when is_list(signal) do
         measurements = add_telemetry_measurements(measurements)
         signal
-        |> wrap(@start)
+        |> prepend_action(@start)
         |> :telemetry.execute(measurements, metadata)
         measurements.systime
     end
 
-    def stop(signal, clock, metadata \\ %{},  measurements \\ %{}) do
+    def stop(signal, clock, metadata \\ %{},  measurements \\ %{}) when is_list(signal) do
         measurements = add_telemetry_measurements(measurements) |> compute_duration(clock)
         signal
-        |> wrap(@stop)
+        |> prepend_action(@stop)
         |> :telemetry.execute(measurements, metadata)
+        measurements.duration
     end
 
-    defp wrap(signal, path) when is_atom(signal) and is_list(path) do
-        List.wrap(@prefix) ++ List.wrap(path) ++ List.wrap(signal)
+    # Convert a module to a list of atoms
+    # representing the module telemetry path
+    # Example: Signal.Stream.Producer -> [:signal, :stream, :producer]
+    def path(module) when is_atom(module) do
+        module
+        |> Module.split()
+        |> Enum.map(&(String.downcase(&1) |> String.to_atom()))
+    end
+
+    defp prepend_action(path, signal) when is_atom(signal) and is_list(path) when is_list(path) do
+        List.wrap(path) ++ List.wrap(signal)
     end
 
     defp compute_duration(%{systime: systime}=measurements, clock) do
         duration = systime - clock
         Map.put(measurements, :duration, duration)
+    end
+
+    defmacro telemetry_start(signal, path) do
+        quote do
+            Signal.Telemetry.start(unquote(signal), unquote(path))
+        end
     end
 
     # Add common telemetry metadata
@@ -35,6 +50,12 @@ defmodule Signal.Telemety do
         |> Map.put(:pid, self())
         |> Map.put(:clock, clock)
         |> Map.put(:systime, systime)
+    end
+
+    defmacro __using__(_) do
+        quote location: :keep do
+            require Signal.Telemetry
+        end
     end
 
 end
