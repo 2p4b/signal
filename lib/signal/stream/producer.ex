5110d19c8859eb57c59d9074aa1eeeff4eaeeb2b
mfoncho <akonumboche@gmail.com>
2024-04-12 14:59:03 -0600

    Refactor reply values in Aggregate module, Added Telemetry to producer and refactored the action process pipline

--- lib/signal/stream/producer.ex
+++ lib/signal/stream/producer.ex
@@ -1,5 +1,6 @@
 defmodule Signal.Stream.Producer do
     use GenServer, restart: :transient
+    use Signal.Telemetry
 
     alias Signal.Multi
     alias Signal.Timer
@@ -45,10 +46,14 @@ defmodule Signal.Stream.Producer do
     end
 
     @impl true
-    def handle_call({:stage, action, events}, from, %Producer{}=state)
+    def handle_call({:stage, action, events}, from, %Producer{}=producer)
     when is_list(events) do
 
-        %Producer{app: app} = state
+        %Producer{app: app} = producer
+
+        meta = metadata(producer, action)
+        measure = measurements(producer)
+        start = telemetry_start(:state, meta, measure)
 
         channel =
             Task
@@ -66,7 +71,7 @@ defmodule Signal.Stream.Producer do
                 end
             end)
 
-        stage = stage_events(state, action, events, channel.pid)
+        stage = stage_events(producer, action, events, channel.pid)
 
         %Stage{version: version} = stage
 
@@ -75,74 +80,95 @@ defmodule Signal.Stream.Producer do
         # Halt until the task is resolved
         case Task.yield(channel, :infinity) do
             {:ok, {:ok, ^version}} ->
-                {:noreply, %Producer{state | index: version}, Timer.seconds(5)}
+                producer = %Producer{producer | index: version}
+                meta = metadata(producer, action) 
+                telemetry_stop(:state, start, meta, measure)
+                {:noreply, producer, Timer.seconds(5)}
 
             {:ok, {:rollback, _}} ->
-                {:noreply, calibrate(state), Timer.seconds(5)}
+                producer = calibrate(producer)
+                meta = metadata(producer, action) 
+                telemetry_stop(:state, start, meta, measure)
+                {:noreply, producer, Timer.seconds(5)}
 
              _ ->
-                {:noreply, calibrate(state), Timer.seconds(5)}
+                producer = calibrate(producer)
+                meta = metadata(producer, action) 
+                telemetry_stop(:state, start, meta, measure)
+                {:noreply, producer, Timer.seconds(5)}
         end
     end
 
     @impl true
     def handle_call({:process, %Action{}=action}, _from, %Producer{}=producer) do
-
-        %Producer{stream: stream, app: app} = producer
-
         %Action{
             result: result,
             command: command, 
         } = action
 
-        aggregate = aggregate_state(producer, Signal.Sync.sync(command, result))
+        meta = metadata(producer, action)
 
-        event_streams =
+        start = telemetry_start(:process, meta, measurements(producer))
+
+        result = 
             command
-            |> handle_command(result, aggregate)
+            |> Signal.Sync.sync(result)
+            |> aggregate_state(action, producer)
+            |> handle_command(action, producer)
             |> group_events_by_stream()
+            |> stage_event_streams(action, producer)
+            |> commit_staged_transaction(action, producer)
 
-        if is_map(event_streams) do
-            case stage_event_streams(producer, action, event_streams) do
-                {:ok, staged} ->
-                    transaction = Transaction.new(staged)
-                    case Writer.commit(app, transaction, []) do
-                        :ok ->
-                            confirm_staged(staged)
+        case result do
+            {:ok, %{index: index, history: history}} ->
+                producer = %Producer{producer | index: index}
+                telemetry_stop(:process, start, meta, measurements(producer))
+                {:reply, {:ok, history}, producer, Timer.seconds(5)}
 
-                            # Get stream index and if the producer
-                            # generated no event for its own stream (very unlikely)
-                            # then restore producer stream index
-                            index = Enum.find_value(staged, producer.index, fn
-                                %{stream: ^stream, version: position} ->
-                                    position
-                                _ -> false
-                            end)
-
-                            histories = Enum.map(staged, fn staged_stream ->
-                                struct(History, Map.from_struct(staged_stream))
-                            end)
-
-                            state = %Producer{producer| index: index}
-
-                            {:reply, {:ok, histories}, state, Timer.seconds(5)}
-
-                        error ->
-                            rollback_staged(staged, error)
-                            {:reply, error, producer, Timer.seconds(5)}
-                    end
-
-                {:error, reason} ->
-                    {:reply, {:error, reason}, producer, Timer.seconds(5)}
-            end
-        else
-            {:reply, event_streams, producer, Timer.seconds(5)}
+            {:error, reason} ->
+                telemetry_stop(:process, start, meta, measurements(producer))
+                {:reply, {:error, reason}, producer, Timer.seconds(5)}
         end
     end
 
-    def stage_event_streams(%Producer{}=producer, action, stream_events)
+    def commit_staged_transaction({:ok, staged}, %Action{}=action, %Producer{}=producer) do
+        %Producer{app: app, stream: stream} = producer
+        meta = metadata(producer, action)
+        measure = measurements(producer)
+        start = telemetry_start(:commit, meta, measure)
+        transaction = Transaction.new(staged)
+        case Writer.commit(app, transaction, []) do
+            :ok ->
+                confirm_staged(staged)
+
+                index = Enum.find_value(staged, producer.index, fn
+                    %{stream: ^stream, version: position} ->
+                        position
+                    _ -> false
+                end)
+
+                history = Enum.map(staged, fn staged_stream ->
+                    struct(History, Map.from_struct(staged_stream))
+                end)
+
+                telemetry_stop(:commit, start, meta, measure)
+                {:ok, %{index: index, history: history}}
+
+            error ->
+                rollback_staged(staged, error)
+                meta = Map.put(meta, :error, error)
+                telemetry_stop(:commit, start, meta, measure)
+                {:error, error}
+        end
+    end
+
+    def stage_event_streams(stream_events, action, %Producer{}=producer)
     when is_map(stream_events) do
         %Producer{app: app, stream: stream} = producer
+
+        meta = metadata(producer, action)
+        measure = measurements(producer)
+        start = telemetry_start(:stage_stream, meta, measure)
 
         task_supervisor = Signal.Application.supervisor(Task, app)
 
@@ -177,12 +203,19 @@ defmodule Signal.Stream.Producer do
 
         case Enum.find(stream_stages, :ok, &Result.error?/1) do
             :ok ->
+                telemetry_stop(:stage_stream, start, meta, measure)
                 {:ok, stream_stages}
 
             {:error, reason} ->
                 rollback_staged(stream_stages, reason)
+                meta = Map.put(meta, :error, reason)
+                telemetry_stop(:stage_stream, start, meta, measure)
                 {:error, reason}
         end
+    end
+
+    def stage_event_streams(error, _action, %Producer{}) do
+        error
     end
 
     def stage_events(producer, action, events) do
@@ -212,7 +245,7 @@ defmodule Signal.Stream.Producer do
         |> GenServer.call({:process, action}, :infinity)
     end
 
-    defp calibrate(%Producer{app: app, stream: {stream_id, _}}=prod) do
+    def calibrate(%Producer{app: app, stream: {stream_id, _}}=prod) do
         case Signal.Store.Adapter.stream_position(app, stream_id) do
             nil ->
                 %Producer{prod | index: 0}
@@ -222,7 +255,10 @@ defmodule Signal.Stream.Producer do
         end
     end
 
-    defp aggregate_state(%Producer{}=producer, sync) do
+    def aggregate_state(sync, %Action{}=action, %Producer{}=producer) do
+        meta = metadata(producer, action)
+        measure = measurements(producer)
+        start = telemetry_start(:state, meta, measure)
         %Producer{app: app, stream: stream, index: index} = producer
         state_opts =
             case sync do
@@ -241,57 +277,83 @@ defmodule Signal.Stream.Producer do
                 _  ->
                     raise(ArgumentError, "invalid stream sync: #{sync}")
             end
-        Signal.Aggregates.Supervisor.prepare_aggregate(app, stream)
-        |> Signal.Aggregates.Aggregate.state(state_opts)
+        state =
+            Signal.Aggregates.Supervisor.prepare_aggregate(app, stream)
+            |> Signal.Aggregates.Aggregate.state(state_opts)
+        telemetry_stop(:state, start, meta, measure)
+        state
     end
 
-    defp handle_command(command, result, aggregate) when is_struct(command) do
+    def handle_command({:ok, aggregate}, %Action{command: command, result: result}=action, %Producer{}=producer) 
+    when is_struct(command) do
+        meta = metadata(producer, action)
+        measure = measurements(producer)
+        start = telemetry_start(:handle, meta, measure)
         try do
             case Handler.handle(command, result, aggregate) do
                 nil ->
+                    telemetry_stop(:handle, start, meta, measure)
                     []
 
                 %Multi{events: events} ->
+                    telemetry_stop(:handle, start, meta, measure)
                     events
 
                 {:ok, event} when is_struct(event) ->
+                    telemetry_stop(:handle, start, meta, measure)
                     List.wrap(event)
 
                 {:ok, event} when is_list(event) ->
+                    telemetry_stop(:handle, start, meta, measure)
                     event
 
                 event when is_struct(event) ->
+                    telemetry_stop(:handle, start, meta, measure)
                     List.wrap(event)
 
                 event when is_list(event) ->
+                    telemetry_stop(:handle, start, meta, measure)
                     event
 
                 {:error, reason} ->
+                    meta = Map.put(meta, :error, reason)
+                    telemetry_stop(:handle, start, meta, measure)
                     Result.error(reason)
             end
         rescue
             raised ->
-                {:error, :raised, {raised, __STACKTRACE__}}
+                error = {:raised, raised, __STACKTRACE__}
+                meta = Map.put(meta, :error, error)
+                telemetry_stop(:handle, start, meta, measure)
+                {:error, error}
+
         catch
             thrown ->
-                {:error, :threw, {thrown, __STACKTRACE__}}
+                error = {:threw, thrown, __STACKTRACE__}
+                meta = Map.put(meta, :error, error)
+                telemetry_stop(:handle, start, meta, measure)
+                {:error, error}
         end
     end
 
+    def handle_command(error, %Action{}, %Producer{}) do
+        error
+    end
+
     # Pass through function
-    defp group_events_by_stream(events) when is_list(events) do
+    def group_events_by_stream(events) when is_list(events) do
         try do
             Enum.group_by(events, &event_stream!/1)
         rescue
             raised ->
-                {:error, :raised, {raised, __STACKTRACE__}}
+                {:error, {:raised, raised, __STACKTRACE__}}
         catch
             thrown ->
-                {:error, :threw, {thrown, __STACKTRACE__}}
+                {:error, {:threw, thrown, __STACKTRACE__}}
         end
     end
 
-    defp group_events_by_stream(unknown) do
+    def group_events_by_stream(unknown) do
         unknown
     end
 
@@ -316,7 +378,7 @@ defmodule Signal.Stream.Producer do
         end
     end
 
-    defp confirm_staged(staged) do
+    def confirm_staged(staged) do
         Enum.each(staged, fn %Stage{version: version, stage: stage}->
             Process.send(stage, {:ok, version}, [])
         end)
@@ -335,6 +397,20 @@ defmodule Signal.Stream.Producer do
             _ ->
                 nil
         end)
+    end
+
+    def metadata(%Producer{}=producer, %Action{}=action) do
+        %{
+            app: producer.app,
+            stream: producer.stream,
+            action: action.command.__struct__,
+        }
+    end
+
+    def measurements(%Producer{}=producer) do
+        %{
+            index: producer.index,
+        }
     end
 
     @impl true
